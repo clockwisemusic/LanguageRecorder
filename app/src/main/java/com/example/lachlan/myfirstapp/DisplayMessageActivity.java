8713b02834a99338d1697448aac5d82dc6a46f07
Lachlan Barclay <clockwise.music@gmail.com>
2015-04-26 20:51:54 +1000

    added settings page

--- app/src/main/java/com/example/lachlan/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/lachlan/myfirstapp/DisplayMessageActivity.java
@@ -38,8 +38,11 @@ public class DisplayMessageActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             return true;
         }
 
